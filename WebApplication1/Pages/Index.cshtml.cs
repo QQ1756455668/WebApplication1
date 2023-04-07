a2c522669103542f7b6174613ba339112546421a
abc <abc@DESKTOP-QG2QTD2>
2023-04-07 09:09:56 +0800

    添加项目文件。

---
+++ WebApplication1/Pages/Index.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            ViewData["test1"] = "test1";
+            ViewData["test2"] = "测试2";
+        }
+    }
+}
